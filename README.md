27e47cad7e338d504fc8d47e9e47b24779162c92
evelyne-dalma <evelyne.phich@dalma.co>
2022-11-28 14:05:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# How to install
+```
+yarn add -D @evelyne-phich/eslint-config-front
+```
+or
+```
+npm install --dev @evelyne-phich/eslint-config-front
+```
+
+# How to config
+- Create a file named `.eslintrc`
+- Copy/paste this code inside :
+```
+{
+  "extends": [
+    "@evelyne-phich/eslint-config-front"
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,60 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "import",
+    "jsx-a11y",
+    "react-hooks",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/no-default-export": "warn",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.stories.@(ts|tsx)"],
+      rules: {
+        "import/no-default-export": "off",
+      },
+    },
+  ],
+};
